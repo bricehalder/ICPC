933469e2a81e0cab4c1834976caacebd02d665c7
Brice Halder <bhalder2@jhu.edu>
2021-03-01 09:33:53 -0500

    shortening + templates

--- template.cpp
+++ template.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
 /* Compile:  g++ -std=c++11 -O2 -Wall template.cpp -o template */
+
+void search(int i) { return; }
 
 int main() {
     /* File I/O if needed */
@@ -31,5 +43,26 @@ int main() {
     while (cin >> x) {
         // code
     }
+
+    /* Shortening examples */
+    int x1, x2, y1, y2, i;
+    vector<pair<int, int>> v;
+    v.push_back(make_pair(x1, y1));
+    v.push_back(make_pair(x2, y2));
+    int d = v[i].first + v[i].second;
+
+    vector<pi> v;
+    v.PB(MP(x1, y1));
+    v.PB(MP(x2, y2));
+    int d = v[i].F + v[i].S;
+    
+    for (int i = 0; i < 100; i++) {
+        search(i);
+    }
+
+    REP(i, 0, 100) {
+        search(i);
+    }
+
     // solution comes here
 }
