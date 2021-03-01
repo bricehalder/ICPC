de282151cd7ffcca36557d43b7022227d6ab9ccd
Brice Halder <bhalder2@jhu.edu>
2021-03-01 03:16:52 -0500

    template

--- template.cpp
+++ template.cpp
@@ -2,10 +2,34 @@
 
 using namespace std;
 
-// Compile:  g++ -std=c++11 -O2 -Wall template.cpp -o template
+/* Compile:  g++ -std=c++11 -O2 -Wall template.cpp -o template */
 
 int main() {
+    /* File I/O if needed */
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    /* Basic input  */
+    int a, b;
+    string x;
+    cin >> a >> b >> x;
+
+    /* scanf and printf a little bit faster than cin/cout */
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int a = 123, b = 456;
+    printf("%d %d\n", a, b);
+
+    string s;
+    getline(cin, s);
+
+    /* Unknown amount of data */
+    while (cin >> x) {
+        // code
+    }
     // solution comes here
 }
