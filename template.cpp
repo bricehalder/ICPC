2897ab5c361e9434d32be6d22e449d22e06a10a6
Brice Halder <bhalder2@jhu.edu>
2021-03-01 03:13:03 -0500

    template

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Compile:  g++ -std=c++11 -O2 -Wall template.cpp -o template
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // solution comes here
+}
